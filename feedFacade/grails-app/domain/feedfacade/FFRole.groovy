cf4eaf08a3aa59ee7c241b8c2b20b528f5543586
Ian Ibbotson <ian.ibbotson@gmail.com>
2016-12-10 11:14:35 +0000

    install spring security core

---
+++ feedFacade/grails-app/domain/feedfacade/FFRole.groovy
@@ -0,0 +1,21 @@
+package feedfacade
+
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+@EqualsAndHashCode(includes='authority')
+@ToString(includes='authority', includeNames=true, includePackage=false)
+class FFRole implements Serializable {
+
+	private static final long serialVersionUID = 1
+
+	String authority
+
+	static constraints = {
+		authority blank: false, unique: true
+	}
+
+	static mapping = {
+		cache true
+	}
+}
