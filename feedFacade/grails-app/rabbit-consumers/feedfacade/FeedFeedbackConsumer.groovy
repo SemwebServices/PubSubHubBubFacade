55c2959835c7c5583507822b1bf102a2afe9911e
Ian Ibbotson <ianibbo@gmail.com>
2019-11-17 11:03:22 +0000

    Add transaction to FeedFeedbackConsumer

--- feedFacade/grails-app/rabbit-consumers/feedfacade/FeedFeedbackConsumer.groovy
+++ feedFacade/grails-app/rabbit-consumers/feedfacade/FeedFeedbackConsumer.groovy
@@ -14,22 +14,25 @@ class FeedFeedbackConsumer {
     log.debug("FeedFeedbackConsumer::handleMessage() - ${context.envelope.routingKey} ${body.message}");
     String[] components = context.envelope.routingKey.split('\\.');
 
+
     if ( components.length > 1 ) {
-      SourceFeed sf = SourceFeed.findByUriname(components[1])
-      Entry entry = null;
-      if ( ( components.length == 3 ) && ( ! components[2].equalsIgnoreCase('unknown') ) ) {
-        entry = Entry.findById(components[2]);
-      }
+      SourceFeed.withTransaction {
+        SourceFeed sf = SourceFeed.findByUriname(components[1])
+        Entry entry = null;
+        if ( ( components.length == 3 ) && ( ! components[2].equalsIgnoreCase('unknown') ) ) {
+          entry = Entry.findById(components[2]);
+        }
   
-      if ( ( sf != null ) && ( body != null ) ) {
-        String body_as_json = groovy.json.JsonOutput.toJson(body)
+        if ( ( sf != null ) && ( body != null ) ) {
+          String body_as_json = groovy.json.JsonOutput.toJson(body)
   
-        FeedEventLog fel = new FeedEventLog(
-                                   ownerFeed:sf,
-                                   entry:entry,
-                                   eventTs:System.currentTimeMillis(),
-                                   eventDetails:body_as_json,
-                                   message:body.message).save(flush:true, failOnError:true);
+          FeedEventLog fel = new FeedEventLog(
+                                     ownerFeed:sf,
+                                     entry:entry,
+                                     eventTs:System.currentTimeMillis(),
+                                     eventDetails:body_as_json,
+                                     message:body.message).save(flush:true, failOnError:true);
+        }
       }
     }
     else {
