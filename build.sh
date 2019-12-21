fa97c48b61527242c4c1a6cb8228d2fdf157a2e5
Ian Ibbotson <ianibbo@gmail.com>
2019-12-21 12:16:54 +0000

    Bumping to v2.0.1 which includes /wait command

--- build.sh
+++ build.sh
@@ -2,15 +2,18 @@
 export SDKMAN_DIR="/home/ibbo/.sdkman"
 [[ -s "/home/ibbo/.sdkman/bin/sdkman-init.sh" ]] && source "/home/ibbo/.sdkman/bin/sdkman-init.sh"
 
+export FF_VER="2.0.1"
+
 sdk use grails 4.0.1
 sdk use java 11.0.5.j9-adpt
 cd feedFacade
 grails clean
 grails prod war
-cp build/libs/feedFacade-2.0.0.war ../docker/feedFacade.war
+cp build/libs/feedFacade-*.war ../docker/feedFacade.war
 cd ../docker
 docker login
-docker build -t semweb/caphub_feedfacade:v2.0 -t semweb/caphub_feedfacade:v2 -t semweb/caphub_feedfacade:latest .
+docker build -t semweb/caphub_feedfacade:v2.0.1 -t semweb/caphub_feedfacade:v2.0 -t semweb/caphub_feedfacade:v2 -t semweb/caphub_feedfacade:latest .
+docker push semweb/caphub_feedfacade:v2.0.1
 docker push semweb/caphub_feedfacade:v2.0
 docker push semweb/caphub_feedfacade:v2
 docker push semweb/caphub_feedfacade:latest
