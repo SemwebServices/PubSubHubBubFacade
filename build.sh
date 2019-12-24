a45edb9d469205a392eefef54b3177a4e33c7f83
Ian Ibbotson <ianibbo@gmail.com>
2019-12-24 11:11:52 +0000

    more work on build script

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ export SDKMAN_DIR="/home/ibbo/.sdkman"
 
 export FF_VER=`grep appVersion ./feedFacade/gradle.properties | cut -f2 -d=`
 
+echo Release FeedFacade $FF_VER
+
 sdk use grails 4.0.1
 sdk use java 11.0.5.j9-adpt
 cd feedFacade
@@ -12,12 +14,18 @@ grails prod war
 cp build/libs/feedFacade-*.war ../docker/feedFacade.war
 cd ../docker
 docker login
-docker build -t semweb/caphub_feedfacade:v$FF_VER -t semweb/caphub_feedfacade:v2.0 -t semweb/caphub_feedfacade:v2 -t semweb/caphub_feedfacade:latest .
-docker push semweb/caphub_feedfacade:v$FF_VER
-docker push semweb/caphub_feedfacade:v2.0
-docker push semweb/caphub_feedfacade:v2
-docker push semweb/caphub_feedfacade:latest
 
-
-
-# See https://medium.com/@krishnaregmi/wait-for-it-docker-compose-f0bac30f3357 for wait-for-it info
+if [[ "$FF_VER" == *-SNAPSHOT ]]
+then
+  echo  SNAPSHOT release - only tagging :v$FF_VER and :latest
+  docker build -t semweb/caphub_feedfacade:v$FF_VER -t semweb/caphub_feedfacade:latest .
+  docker push semweb/caphub_feedfacade:v$FF_VER
+  docker push semweb/caphub_feedfacade:latest
+else
+  echo  Standard Release
+  docker build -t semweb/caphub_feedfacade:v$FF_VER -t semweb/caphub_feedfacade:v2.0 -t semweb/caphub_feedfacade:v2 -t semweb/caphub_feedfacade:latest .
+  docker push semweb/caphub_feedfacade:v$FF_VER
+  docker push semweb/caphub_feedfacade:v2.0
+  docker push semweb/caphub_feedfacade:v2
+  docker push semweb/caphub_feedfacade:latest
+fi
